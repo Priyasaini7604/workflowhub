3e9d5b6859a03aeb93b555649b10b9352569d8e8
Priyasaini7604 <priyasaini1644@gmail.com>
2026-06-09 16:55:28 +0530

    fix: removed trailing whitespace from models

--- backend/onboarding/models.py
+++ backend/onboarding/models.py
@@ -81,8 +81,7 @@ class OnboardingChecklist(models.Model):
     # --- Audit Information ---
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-     
-    # soft Delete----
+    # ----soft Delete----
     is_archived = models.BooleanField(default=False)
     archived_at = models.DateTimeField(null=True, blank=True)
 
