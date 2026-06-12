528fb6899873fb265873ea3ec2410038739bb58b
Priyasaini7604 <priyasaini1644@gmail.com>
2026-06-12 13:49:46 +0530

    fix: removed duplicate RefreshToken import

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
 from .models import User
