ec35cffefe7862ef5114e28aa3d97fa541aade20
Priyasaini7604 <priyasaini1644@gmail.com>
2026-06-12 13:42:46 +0530

    fix: user/urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import RegisterView, LoginView, MeView, UserDeactivateView
-from rest_framework_simplejwt.views import TokenRefreshView, LogoutView
+from .views import RegisterView, LoginView, MeView, UserDeactivateView, LogoutView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
