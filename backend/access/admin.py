37ba47bb95dc52d91772354be8085593d5bd62f3
Priyasaini7604 <priyasaini1644@gmail.com>
2026-07-24 16:07:41 +0530

    Remove unsed imports

--- backend/access/admin.py
+++ backend/access/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import SoftwareAccess
 
-# Register your models here.
+
+@admin.register(SoftwareAccess)
+class SoftwareAccessAdmin(admin.ModelAdmin):
+    list_display = (
+        'employee',
+        'software_name',
+        'status',
+        'granted_on',
+        'revoked_on',
+        'is_archived')
+    list_filter = ('status', 'is_archived')
+    search_fields = (
+        'software_name',
+        'employee__first_name',
+        'employee__last_name')
--- backend/access/views.py
+++ backend/access/views.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
-from rest_framework import generics, permissions
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import SoftwareAccess
 from .serializers import (
     SoftwareAccessSerializer,
